9aa8fd0668c940f86213f870b1b7096148b682c6
Santhosh V <santhosh.v6426@gmail.com>
2020-10-21 16:58:14 +0530

    [Santhosh] Add, Added Program to Check Employee Attendance.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿/*
+ *  C# CODING STANDARDS - LINE SPACE
+ * NOTE - A SINGLE BLANK LINE SEPARATES THE TWO BLOCKS i.e. USING STATEMENTS, CLASS DECLARATION, etc
+ */
+
+/******************************************************************************
+ *  Compilation:  C:\Users\Santhu\source\repos\EmployeeWageComputation_C\bin\Debug\netcoreapp3.1\EmployeeWageComputation_C.exe
+ *  Execution:    EmployeeWageComputation_C.exe
+ *  
+ *  Purpose: Computes Employee Wages.
+ *
+ *  @author  Santhosh V
+ *  @version 1.0
+ *  @since   06-08-2017
+ *
+ ******************************************************************************/
+
+/// Including the requried assemblies in to the program
+using System;
 
 namespace EmployeeWageComputation_C
 {
@@ -7,6 +25,18 @@ namespace EmployeeWageComputation_C
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program.");
+            /// Generate random number
+            Random r = new Random();
+            int randomNumber=r.Next(0, 2);
+            /// Condition to Check if Employee is Present or Absent.
+            /// Displays Employee is present or Absent based on Random Number.
+            if (randomNumber == 1)
+            {
+                Console.WriteLine("Employee is Present!");
+            }
+            else {
+                Console.WriteLine("Employee is Absent!");
+            }
         }
     }
 }
