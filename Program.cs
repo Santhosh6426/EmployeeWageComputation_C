f380c2a4cb5a99dd67f20a387de2a61d7b998e39
Santhosh V <santhosh.v6426@gmail.com>
2020-10-21 16:35:14 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWageComputation_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program.");
+        }
+    }
+}
